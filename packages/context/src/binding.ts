1519452251421fb949c2eba1d1e8bfa420f2c2fa
Raymond Feng <enjoyjava@gmail.com>
2018-01-21 10:25:21 -0800

    feat(context): support binding config and @inject.config
    1. Add context.configure to configure bindings
    2. Add context.getConfig to look up configuration for a binding
    3. Add @injection.config to receive injection of configuration

--- packages/context/src/binding.ts
+++ packages/context/src/binding.ts
@@ -283,6 +283,9 @@ export class Binding<T = BoundValue> {
     );
   }
 
+  /**
+   * Lock the binding so that it cannot be rebound
+   */
   lock(): this {
     this.isLocked = true;
     return this;
@@ -548,6 +551,9 @@ export class Binding<T = BoundValue> {
     return this;
   }
 
+  /**
+   * Convert to a plain JSON object
+   */
   toJSON(): Object {
     // tslint:disable-next-line:no-any
     const json: {[name: string]: any} = {
